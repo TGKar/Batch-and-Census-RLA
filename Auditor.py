compmoveseat margin calculation fix

Auditor.py
@@ -112,9 +112,18 @@ class Auditor:
                     print("A WRONG ASSERTION WAS APPROVED!!!")
                 batch_prediction.append(0)  # TODO
                 #batch_prediction.append(assertions[delete_ind - i].get_batch_prediction())
-                print("Finished assertion: ", str(assertions[delete_ind - i]), ' with margin ',  assertions[delete_ind - i].vote_margin,'after ballot ', str('{:,}'.format(ballot_counter)),
-                      "True mean:", assorter_true_mean, '. batch count ', batch_counter,
-                      " Prediction: ", batch_prediction[-1])
+                """
+                if assertions[delete_ind - i].type == 3:  # TODO delete later
+                    true_margin = assertions[delete_ind - i].true_vote_margin
+                    closer_margin = assertions[delete_ind - i].closer_margin
+                else:
+                    true_margin = '-'
+                    closer_margin = '-'
+                """
+
+                print("Finished assertion: ", str(assertions[delete_ind - i]), ' with reported margin ',
+                      assertions[delete_ind - i].vote_margin, 'after ballot ', str('{:,}'.format(ballot_counter)),
+                      '. batch count ', batch_counter, " Prediction: ", batch_prediction[-1])
                 batch_prediction_diff.append(batch_prediction[-1] - batch_counter)
 
                 if assertions[delete_ind - i].eta.assorter_sum / assertions[delete_ind - i].eta.total_ballots < 0.5:
@@ -133,18 +142,17 @@ class Auditor:
             batch_probs[batch_ind] = 0  # Remove audited batch from sampling pool
             if batch_counter + 1 <= len(self.election_profile.batches):
                 batch_probs /= np.sum(batch_probs)  # Normalize the distribution
-        print("Remaining assertions:")
-        for assertion in assertions:
-            print(str(assertion) + ". T:" + str(assertion.T) + '. Margin: ' + str(assertion.vote_margin) +
-                  ". Reported assorter value: " + str(assertion.reported_assorter_mean) + '. Actual mean value: ' +
-                  str(assertion.eta.assorter_sum / assertion.eta.total_ballots) + '. Final eta assorter mean: ' + str(assertion.eta.assorter_sum / assertion.eta.total_ballots))
-            print('Ballots examined: ', str(assertion.eta.total_ballots), '/', str(assertion.eta.total_ballots))
+        if len(assertions) > 0:
+            print("Remaining assertions:")
+            for assertion in assertions:
+                print(str(assertion) + ". T:" + str(assertion.T) + '. Margin: ' + str(assertion.vote_margin) +
+                      ". Reported assorter value: " + str(assertion.reported_assorter_mean) + '. Actual mean value: ' +
+                      str(assertion.eta.assorter_sum / assertion.eta.total_ballots) + '. Final eta assorter mean: ' + str(
+                    assertion.eta.assorter_sum / assertion.eta.total_ballots))
+                print('Ballots examined: ', str(assertion.eta.total_ballots), '/', str(assertion.eta.total_ballots))
 
-
-        print(np.mean(batch_prediction_diff))
-        print(np.std(batch_prediction_diff))
-        #plt.hist(batch_prediction_diff)
-        #plt.show()
+        else:
+            print('Results approved!')
 
         return len(assertions) == 0, finished_assertions
 

Plotter.py
@@ -105,7 +105,7 @@ def assertions_with_error_plots(assertions_list, noised_assertions_list, total_v
     max_margin = max(assertion_data_mat[:, MARGIN_IND])
 
     # Plot comparison
-    fig, axs = plt.subplots(3, 1, sharex=True, sharey=True)
+    fig, axs = plt.subplots(3, 1, sharex=True)
     for i, lab in enumerate(ASSERTION_LABELS):
         slicer = np.where(assertion_data_mat[:, ASSERTION_TYPE_IND] == i + 1)
         axs[0].scatter(assertion_data_mat[slicer, MARGIN_IND], assertion_data_mat[slicer, ALPHA_REQ_BALLOTS_IND], label=lab)
@@ -114,22 +114,19 @@ def assertions_with_error_plots(assertions_list, noised_assertions_list, total_v
     axs[1].plot([0, max_margin], [total_voters, total_voters], '--', label='Total Voters')
     axs[0].set_xlim((0, max_margin + total_voters / 10**5))
     axs[0].legend()
-    fig.suptitle("Knesset " + str(knesset_num) + ' - Required # of Ballots vs. Assorter Margin')
+    fig.suptitle("Knesset " + str(knesset_num) + ' - Batchcomp Required # of Ballots vs. Assorter Margin')
     axs[0].set_title("Without Counting Errors")
-    axs[1].set_title("With p=0.01 Probability of Counting Error")
+    axs[1].set_title("With Counting Errors")
     fig.supxlabel("Assertion Margin")
     fig.supylabel("Required Ballots")
 
     # Plot Difference
-    diff = assertion_data_mat[:, ALPHA_REQ_BALLOTS_IND] - assertion_data_mat[:, BATCHCOMP_REQ_BALLOTS_IND]
+    diff = assertion_data_mat[:, BATCHCOMP_REQ_BALLOTS_IND] - assertion_data_mat[:, ALPHA_REQ_BALLOTS_IND]
     for i, lab in enumerate(ASSERTION_LABELS):
         slicer = np.where(assertion_data_mat[:, ASSERTION_TYPE_IND] == i + 1)
         axs[2].scatter(assertion_data_mat[slicer, MARGIN_IND], diff[slicer], label=lab)
         axs[2].plot([0, max_margin], [0, 0], '--')
-    axs[2].set_title(
-        "Knesset " + str(knesset_num) + 'Difference')
-    plt.xlabel("Assertion Margin")
-    plt.ylabel("Required Ballots")
+    axs[2].set_title('Difference (With Errors - Without Errors)')
     plt.show()
 
 def prediction_plots(assertions_lists):